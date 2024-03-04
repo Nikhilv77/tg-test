99563cba6ef7453471cf3335ee147e9e4ab2e224
Nikhil <Nv581339@gmail.com>
2024-03-04 22:06:39 +0530

    bug fixing

--- src/home-sections/HeroSection.js
+++ src/home-sections/HeroSection.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import styled from 'styled-components'
 import Header from '../components/header/Header'
 
 
 const HeroSection = () => {
   return (
-    <>
       <Header />
-</>
   )
 }
 
