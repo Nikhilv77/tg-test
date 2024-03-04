ed641eec5e22bfde72031a4a9ad7b96a1bf4b597
Nikhil <Nv581339@gmail.com>
2024-03-05 00:29:02 +0530

    fixing

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -13,7 +13,7 @@ const HomePage = () => {
   return (
     <div>
       <HomeNavbar />
-     {/* <Header/> */}
+     <Header/>
       <FirstSection />
       <SecondSection />
       <ThirdSection />
